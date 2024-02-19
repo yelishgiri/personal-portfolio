b2fbf077faf132ae6d610430f10ee1e1ca15064c
Yelish <118233081+GrapeJuice11111@users.noreply.github.com>
2024-02-19 11:00:39 -0600

    Fix The Subdirectory Structure To Main Directory

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  <>
+    <Navbar />
+    <About />
+    <Projects />
+    <Skills />
+    <Contact />
+  </>;
+}
+
+export default App;
