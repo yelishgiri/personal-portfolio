4365883e8ac196aefcb05504a676f56181ac0237
Yelish <118233081+GrapeJuice11111@users.noreply.github.com>
2024-02-20 02:32:27 -0600

    Create A Basic Layout For The React Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
-
+import About from "./components/About.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Projects from "./components/Projects.jsx";
+import Skills from "./components/Skills.jsx";
+import Contact from "./components/Contact.jsx";
 import "./App.css";
 
 function App() {
-  <>
-    <Navbar />
-    <About />
-    <Projects />
-    <Skills />
-    <Contact />
-  </>;
+  return (
+    <main>
+      <About />
+      <Navbar />
+      <Skills />
+      <Projects />
+      <Contact />
+    </main>
+  );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/App.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
